d589656ab6b25eafb6e394a6b51b4ac14080f3d1
Jerry Lott <lottjerry@gmail.com>
2025-04-25 03:33:11 -0500

    Added upload csv route that returns custom data

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+import pandas as pd
+from io import StringIO
 from fastapi.middleware.cors import CORSMiddleware
 
 
@@ -18,3 +20,28 @@ app.add_middleware(
 async def root():
     return {"message": "Missing Item Finder API"}
 
+@app.post("/upload-csv/")
+async def upload_csv(file: UploadFile = File(...)):
+    try:
+        contents = await file.read()
+        csv_file = StringIO(contents.decode('utf-8'))
+        df = pd.read_csv(csv_file)
+        df = df.fillna('')
+        df['Units'] = df['Units'].astype(int)
+        
+        unique_values = df['Pack/Size'].unique().tolist()  
+        myDict = {}
+        myDict2 = {}
+
+        for index, i in enumerate(unique_values):
+            myDict[i] = int(df['Units'][df['Pack/Size'] == unique_values[index]].sum())
+            myDict2[i] = df[['Description','Units']][df['Pack/Size'] == unique_values[index]].to_dict()
+        
+        return {
+            #"unique_pack_sizes": unique_values,
+            #"pack_size_counts": value_counts,
+            "size_totals": myDict,
+            "size_details": myDict2
+        }
+    except Exception as e:
+        return {"error": str(e)}
